3d648bed3819812c48f6a2d11a51eadfad2ada9b
Mauritz Skogøy <mauritzs@stud.ntnu.no>
2023-09-18 15:09:48 +0200

    First functional window - javafx GUI

--- src/main/java/gr2349/app/BookApp.java
+++ src/main/java/gr2349/app/BookApp.java
@@ -1,12 +1,36 @@
 package gr2349.app;
 
-//import javafx.application.Application;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 
-public class BookApp {
-    //Add-functionality
+public class BookApp extends Application{
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        launch(); 
+    }
+
+    Button button;
+
+    @Override
+
+    public void start(Stage primaryStage) throws IOException{
+        primaryStage.setTitle("Myfirstwindow");
+        
+        button = new Button("Testbutton");
+        
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout,300,250);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
     }
 }
