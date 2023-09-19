fcd7e9072b3e22ec837974a3c4237cfb3bfa8e28
Fredrik Jakob Talleraas Emblem <fjemblem@stud.ntnu.no>
2023-09-19 09:19:36 +0200

    Added BookEntity

--- src/main/java/gr2349/app/BookApp.java
+++ src/main/java/gr2349/app/BookApp.java
@@ -3,7 +3,7 @@ package main.java.gr2349.app;
 import javafx.application.Application;
 
 
-public class BookApp Application {
+public class BookApp extends Application {
     //Add-functionality
 
 }
