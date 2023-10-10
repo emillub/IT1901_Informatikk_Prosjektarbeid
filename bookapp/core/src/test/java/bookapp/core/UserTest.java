3d9e38e48b1e5921a506daaa45cd4ea49533f2e7
Harald Jentoft Stuge <haraljst@stud.ntnu.no>
2023-10-10 14:23:01 +0200

    Test: Skrevet test til Bruker-klassen

---
+++ bookapp/core/src/test/java/bookapp/core/UserTest.java
@@ -0,0 +1,50 @@
+package bookapp.core;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+
+    private User user;
+    private Book book1;
+
+    @BeforeEach
+    void setUp() {
+        user = new User("John");
+        book1 = new Book("Sample Title 1", "Sample Author 1");
+    }
+
+    @Test
+    void testConstructor() {
+        assertNotNull(user);
+        assertEquals("John", user.getName());
+    }
+
+    @Test
+    void testWriteReview() {
+        user.writeReview(book1, 5);
+
+        // Assuming BookReview's constructor also adds the review to the book's reviews.
+        assertEquals(1, book1.getReviews().size());
+        assertEquals(user, book1.getReviews().get(0).getReviewer());
+    }
+
+    @Test
+    void testWriteMultipleReviewsForSameBook() {
+        user.writeReview(book1, 5);
+        
+        // Trying to write another review for the same book by the same user should throw an exception.
+        // Assuming this based on previous test logic provided.
+        assertThrows(IllegalArgumentException.class, () -> user.writeReview(book1, 4));
+    }
+
+    @Test
+    void testToString() {
+        assertEquals("John", user.toString());
+    }
+    
+    // Add other tests as needed...
+
+}
