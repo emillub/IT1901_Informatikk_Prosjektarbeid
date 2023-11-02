433809a23a22e74a28cdc9c27221a2577d2781ba
emillub <145599402+emillub@users.noreply.github.com>
2023-11-02 09:53:28 +0100

    Test: BookComparator sorting

--- bookapp/core/src/main/java/bookapp/core/BookComparator.java
+++ bookapp/core/src/main/java/bookapp/core/BookComparator.java
@@ -6,13 +6,20 @@ public class BookComparator implements Comparator<Book>{
     protected static final String RATING = "rating";
     protected static final String AUTHOR_NAME = "authorName";
     protected static final String BOOK_TITLE = "ttile";
-    private String sortBy = "";
+    private String sortBy = RATING;
+
+    public BookComparator(){}
+
+    public BookComparator(String sortBy){
+        this.sortBy = sortBy;
+    }
     
     @Override
     public int compare(Book o1, Book o2) {
         if (sortBy.equals(RATING)) {
-            return Float.compare(o1.getAverageRating(), o2.getAverageRating());
+            return Float.compare(o2.getAverageRating(), o1.getAverageRating());
         } else if (sortBy.equals(AUTHOR_NAME)) {
+            System.out.println("Sorting by author");
             return o1.getAuthor().compareTo(o2.getAuthor());
         } 
         return o1.getTitle().compareTo(o2.getTitle());
