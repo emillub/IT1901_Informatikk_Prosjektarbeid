3e39d8d907571c539052f4e662f710390c24d0b5
emillub <145599402+emillub@users.noreply.github.com>
2023-11-15 17:04:21 +0100

    Test: Get booklist from api

--- bookapp/restapi/src/test/java/bookapp/restapi/BookappModelApplicationTest.java
+++ bookapp/restapi/src/test/java/bookapp/restapi/BookappModelApplicationTest.java
@@ -1,5 +1,6 @@
 package bookapp.restapi;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -35,40 +36,42 @@ public class BookappModelApplicationTest {
     @Test
     public void getBooksTest() throws Exception {
         // Create sample data
-        // Book book = new Book("Sample Book", "Sample Author");
-        // List<Book> bookList = Arrays.asList(book);
+        Book book = new Book("Sample Book", "Sample Author");
+        List<Book> bookList = Arrays.asList(book);
 
         // // Setup mock behavior of FileHandlerService
-        //given(fileHandlerService.readBooksFromFile()).willReturn(bookList);
+        given(fileHandlerService.readBooksFromFile()).willReturn(bookList);
 
         // Perform GET request and verify outcome
         mockMvc.perform(get("/api/books/fetchList"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", Matchers.hasSize(1)))
+                .andExpect(jsonPath("$[0].title", Matchers.equalTo(book.getTitle())));
     }
 
     // Test for POST request
-    // @Test
-    // public void postReviewTest() throws Exception {
-    //     // Sample data
-    //     String sampleTitle = "Sample Book";
-    //     User sampleUser = new User("Sample User");
-    //     String jsonPayload = "{\"book\":{\"title\":\"" + sampleTitle + "\",\"author\":\"Sample Author\"},\"reviewer\":{\"name\":\"Sample User\"},\"rating\":5}";
+    @Test
+    public void postReviewTest() throws Exception {
+        // // Sample data
+        // String sampleTitle = "Sample Book";
+        // User sampleUser = new User("Sample User");
+        // String jsonPayload = "{\"book\":{\"title\":\"" + sampleTitle + "\",\"author\":\"Sample Author\"},\"reviewer\":{\"name\":\"Sample User\"},\"rating\":5}";
     
-    //     List<Book> mockBookList = Collections.singletonList(new Book(sampleTitle, sampleUser.getName()));
+        // List<Book> mockBookList = Collections.singletonList(new Book(sampleTitle, sampleUser.getName()));
 
-    //     // Setup mock behavior
-    //     given(fileHandlerService.readBooksFromFile()).willReturn(mockBookList);
-    //     // No need to mock 'updateBookInLibrary' if it returns void, just verify it's called later
+        // // Setup mock behavior
+        // given(fileHandlerService.readBooksFromFile()).willReturn(mockBookList);
+        // // No need to mock 'updateBookInLibrary' if it returns void, just verify it's called later
     
-    //     // Perform POST request and verify outcome
-    //     mockMvc.perform(post("/api/books/post/{bookName}", sampleTitle)
-    //             .contentType(MediaType.APPLICATION_JSON)
-    //             .content(jsonPayload))
-    //             .andExpect(status().isOk())
-    //             .andExpect(content().string("Review successfully added"));
+        // // Perform POST request and verify outcome
+        // mockMvc.perform(post("/api/books/post/{bookName}", sampleTitle)
+        //         .contentType(MediaType.APPLICATION_JSON)
+        //         .content(jsonPayload))
+        //         .andExpect(status().isOk())
+        //         .andExpect(content().string("Review successfully added"));
     
-    //     // Verify interactions with mock
-    //     verify(fileHandlerService).updateBookInLibrary(any(Book.class));
-    // }
+        // // Verify interactions with mock
+        // verify(fileHandlerService).updateBookInLibrary(any(Book.class));
+    }
 
 }
